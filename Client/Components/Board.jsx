73600eeac56dde7dbfb05f14f336b3cc570c7cf3
Donald Twiford <donaldptwiford@gmail.com>
2024-04-09 10:42:10 -0700

    integrate zustand and a sign in

--- Client/Components/Board.jsx
+++ Client/Components/Board.jsx
@@ -1,196 +1,190 @@
-import React, {useState, useEffect, Fragment} from 'react';
+import React, { useEffect } from 'react';
 import Row from './Row.jsx';
+import { useStore } from '../Store/useStore.js';
 
 const Board = () => {
 
-  /* -- STATE -- */
-  const [board, setBoard] = useState({}); // covered
-  const [currentPlayer, changePlayer] = useState('X');
-  const [gameOver, changeGameStatus] = useState(false);
-  const [message, updateMessage] = useState('');
-  const [score, setScore] = useState({'X':0, 'O':0});
-  const [boardSize, changeBoardSize] = useState(3); // covered
-  
-  /* --- HELPER FUNCTIONS --- */
+  // State
+  const {
+    boardSize, setBoardSize,
+    board, setBoard,
+    players, setPlayers,
+    message, setMessage,
+    currentPlayer, setCurrentPlayer,
+    score, setScore,
+    gameOver, setGameOver,
+    gameReady, setGameReady,
+    inactivePlayer
+  } = useStore((state) => state);
 
-  // reset the board but leave the current scores
-  const resetBoard = () => {
-    const board = {};
-    for(let i = 0; i < boardSize; i++){
-      for(let j = 0; j < boardSize; j++){
-        board[`${i}${j}`] = '-';
-      }
-    }
-    board.dashes = Math.pow(boardSize, 2);
-    setBoard(board);
-    changeGameStatus(false);
-    updateMessage('');
-    changePlayer('X');
+  // Functions
+  const setUpPlayers = (event) => {
+    event.preventDefault();
+    const player1Field = document.getElementById('player1-name').value;
+    const player2Field = document.getElementById('player2-name').value;
+    const player1 = player1Field.length ? player1Field : 'X';
+    const player2 = player2Field.length ? player2Field : 'O';
+    setPlayers(player1, player2);
+    setScore({
+      [player1]: 0,
+      [player2]: 0
+    });
+    setGameReady(true);
   };
 
-  // reset the board and current scores
-  const fullReset = () => {
-    resetBoard();
-    setScore({'X':0, 'O':0});
-  };
-
-  // increase board size by 1 (ridiculous but fun)
-  const increaseBoard = () => {
-    if(boardSize < 7) changeBoardSize(boardSize + 1);
-  };
-
-  // decrease board size by 1
-  const decreaseBoard = () => {
-    if(boardSize > 1) changeBoardSize(boardSize - 1);
-  };
-
-  /* --- VICTORY CONDITIONS --- */
-  // reference to player not taking their turn
-  const inactivePlayer = currentPlayer === 'X' ? 'O' : 'X';
-
-  // winner result function
   const winner = () => {
     const newScore = {...score};
     newScore[inactivePlayer]++;
-    // update game status, score, and victory message state
-    changeGameStatus('true');
-    updateMessage(`Player ${inactivePlayer} wins!!!`);
+    setGameOver(true);
+    setMessage(`Player ${players.valueRef[inactivePlayer]} wins!!!`);
     setScore(newScore);
   };
-
-  // draw result function
+  
   const draw = () => {
-    //Update game status and victory message state
-    changeGameStatus('true');
-    updateMessage('It\'s a Draw!!!');
+    setGameOver(true);
+    setMessage('It\'s a Draw!!!');
   };
-
-  // check for winner
+  
   const checkForWinner = () => {
-    const b = {...board};
-    const iP = inactivePlayer;
+    const newBoard = {...board};
     let victoryCondition = ``;
-    for(let i = 0; i < boardSize; i++) {
-      victoryCondition += `${iP}`;
-    }
-
-    // check the rows
-    for(let i = 0; i < boardSize; i++) {
+  
+    for(let counter = 0; counter < boardSize; counter++) {
+      victoryCondition += `${inactivePlayer}`;
+    };
+  
+    for(let row = 0; row < boardSize; row++) {
       let result = '';
-      for(let j = 0; j < boardSize; j++) {
-        result += `${b[`${i}${j}`]}`;
-      }
+      for(let column = 0; column < boardSize; column++) {
+        result += `${newBoard[`${row}${column}`]}`
+      };
       if(result === victoryCondition) return winner();
-    }
+    };
 
-    // check the columns
-    for(let j = 0; j < boardSize; j++) {
+    for(let column = 0; column < boardSize; column++) {
       let result = '';
-      for(let i = 0; i < boardSize; i++) {
-        result += `${b[`${i}${j}`]}`;
-      }
+      for(let row = 0; row < boardSize; row++) {
+        result += `${newBoard[`${row}${column}`]}`;
+      };
       if(result === victoryCondition) return winner();
-    }
-    
-    // check the diagonals
+    };
+
     let diagonal1 = '';
-    for(let i = 0; i < boardSize; i++) {
-      diagonal1 += `${b[`${i}${i}`]}`;
-    }
+    for(let row = 0; row < boardSize; row++) {
+      diagonal1 += `${newBoard[`${row}${row}`]}`;
+    };
     if(diagonal1 === victoryCondition) return winner();
+
     let diagonal2 = '';
-    for(let i = 0; i < boardSize; i++) {
-      diagonal2 += `${b[`${i}${boardSize-1-i}`]}`;
+    for(let row = 0; row < boardSize; row++) {
+      diagonal2 += `${newBoard[`${row}${boardSize-1-row}`]}`;
     }
     if(diagonal2 === victoryCondition) return winner();
 
-    // check for a tie
-    if(board.dashes === 0) return draw();
+    if(newBoard.dashes === 0) return draw();
   };
 
-  // for switching players
-  const switchPlayer = () => {
-    const change = {
-      'X' : 'O',
-      'O' : 'X'
+  const softReset = () => {
+    const newBoard = {};
+    for(let i = 0; i < boardSize; i++){
+      for(let j = 0; j < boardSize; j++){
+        newBoard[`${i}${j}`] = '-';
+      };
     };
-    changePlayer(change[currentPlayer]);
+    newBoard.dashes = Math.pow(boardSize, 2);
+    setBoard(newBoard)
+    setGameOver(false);
+    setMessage('');
+    setCurrentPlayer('X');
+  };
+  
+  const fullReset = () => {
+    softReset();
+    const player1 = players.valueRef['X'];
+    const player2 = players.valueRef['O'];
+    setScore({[player1]: 0, [player2]: 0});
   };
 
-  // for switching the box icon
-  const changeBox = (event) => {
-    if(!gameOver) {
-      const index = event.target.id;
-      const newBoard = {...board}; // make a copy of board
-      if(newBoard[index] === '-') { //use it in place of board
-        newBoard[index] = currentPlayer;
-        newBoard.dashes--;
-        setBoard(newBoard); //when done, change board to match the newBoard
-        switchPlayer();
-      }
-    }
+  const increaseBoard = () => {
+    if(boardSize < 7) setBoardSize(boardSize + 1);
+  };
+  
+  const decreaseBoard = () => {
+    if(boardSize > 3) setBoardSize(boardSize - 1);
   };
 
-  /* --- USEEFFECTS --- */
+  const restartGame = () => {
+    fullReset();
+    setGameReady(false);
+  }
 
-  useEffect(resetBoard, []);
-  useEffect(resetBoard, [boardSize]);
+  useEffect(fullReset, [boardSize]);
   useEffect(checkForWinner, [board]);
 
-  /* --- ROW CREATION --- */
-
-  // generate rows with unique keys that will be used to creat boxes with unique ids
   const rows = [];
+  
   for(let i = 0; i < boardSize; i++){
     rows.push(
-      <div>
+      <div key={`Row-div${i}`}>
         <Row 
           key={`Row${i}`} 
-          index={i} 
-          board={board} 
-          changeBox={changeBox}
-          boardSize={boardSize}
+          index={i}
         />
       </div>
     );
-  }
-
-  /* --- CONDITIONALS --- */
-
-  const endDiv = [];
-  if(message !== '') endDiv.push(<div className='end-game'>{message} <button className='word-glow' id='reset-board' onClick={resetBoard}>Reset Board</button></div>);
-
-  const softReset = [];
-  if(!gameOver) softReset.push(<button className='soft-reset word-glow' onClick={resetBoard}>Soft Reset</button>);
-  
-  /* --- RETURN --- */
+  };
 
   return (
     <div id='game'>
-      <h1>
-        <button className='board-change word-glow' onClick={decreaseBoard}>
-          -
+      { gameReady ?
+        <>
+          <h1>
+            <button className='board-change word-glow' onClick={decreaseBoard}>
+              -
+            </button>
+            Tic Tac Toe
+            <button className='board-change word-glow' onClick={increaseBoard}>
+              +
+            </button>
+          </h1>
+          <h2>
+            Current Player: {players.valueRef[currentPlayer] + ': ' + currentPlayer} 
+            {!gameOver && 
+              <button className='soft-reset word-glow' onClick={softReset}>
+                Soft Reset
+              </button>}
+          </h2>
+          {message && 
+            <div className='end-game'>
+              {message} 
+              <button className='word-glow' id='reset-board' onClick={softReset}>
+                Reset Board
+              </button>
+            </div>}
+          {rows}
+          <h3>SCORES</h3>
+          <p>
+            <span className='player-1'>
+              {`Player: ${players.valueRef['X']}: ${score[players.valueRef['X']]}`}
+            </span>
+            <span className='player-2'>
+              {`Player: ${players.valueRef['O']}: ${score[players.valueRef['O']]}`}
+            </span>
+          </p>
+        <button className='word-glow' id='full-reset' onClick={fullReset}>
+          Full Reset
         </button>
-        Tic Tac Toe
-        <button className='board-change word-glow' onClick={increaseBoard}>
-          +
+        <button className='word-glow' id='full-reset' onClick={restartGame}>
+          Resart Game
         </button>
-      </h1>
-      <h2>Current Player: {currentPlayer} {softReset}</h2>
-      {endDiv}
-      {/* Add in the rows array (with fragments! neat!)*/}
-        {rows}
-      <h3>SCORES</h3>
-      <p>
-        <span className='player-1'>
-          {`Player X: ${score['X']}`}
-        </span>
-        <span className='player-2'>
-          {`Player O: ${score['O']}`}
-        </span></p>
-      <button className='word-glow' id='full-reset' onClick={fullReset}>
-        Full Reset
-      </button>
+      </> :
+      <>
+        <div>
+            <input id='player1-name' type='text'/>
+            <input id='player2-name' type='text'/>
+            <button id="game-start-button" onClick={setUpPlayers}>Start</button>
+        </div>
+      </>}
     </div>
   );
 };
--- Client/Components/Box.jsx
+++ Client/Components/Box.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import { useStore } from '../Store/useStore';
 
 const Box = (props) => {
 
-  // final destructure of drilled props
-  const {board, index, changeBox} = props;
+  const {index} = props;
 
-  // each box references its location in a state grid based in it's unique id
+  const { 
+    board, 
+    gameOver, 
+    currentPlayer,
+    setBoard,
+    switchActivePlayer
+  } = useStore((state) => state);
+
+  const changeBox = (event) => {
+    if(!gameOver) {
+      const index = event.target.id;
+      const newBoard = {...board};
+      if(newBoard[index] === '-') {
+        newBoard[index] = currentPlayer;
+        newBoard.dashes--;
+        setBoard(newBoard);
+        switchActivePlayer();
+      };
+    };
+  };
+
   return (
     <button className='tic-box' id={index} onClick={changeBox}> 
       {board[index]} 
